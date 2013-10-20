adb0bf68979d50786d215123fcd38dd7beff6cc3
Eric <etran029@gmail.com>
2013-10-19 21:11:53 -0700

    2nd commit

--- myScript.js
+++ myScript.js
@@ -1,48 +1,54 @@
-document.writeln("<table>");
-document.writeln("<tr>");
-document.writeln("<td>1</td>");
-document.writeln("<td>2</td>");
-document.writeln("<td>3</td>");
-document.writeln("</tr>");
-
-document.writeln("<tr>");
-document.writeln("<td>11</td>");
-document.writeln("<td>12</td>");
-document.writeln("<td>13</td>");
-document.writeln("</tr>");
-document.writeln("</table>");
-
 function main () {
-      //alert("Let's create a times table!");
-      //var spaces = prompt('Please input a number between 5-20', 12);
+
+      alert("Let's create a times table!");
+      var spaces = prompt('Please input a number between 5-20', 12);
       //alert(spaces);
+      var input = (Number(spaces));
 
 
       var id = document.getElementById('multi');
       var out = "";
-
-
-
-      out += '<tr>';
-      out += '<td></td>';
-      for (var x = 0; x < 20; x++)
+      var valid = input>=5 && input<=20;
+      if(!input || !valid)
       {
-      out += '<td class="yellow">' + (x+1) + '</td>';
+            var error = "Please enter a valid input between 5-20."
+            alert(error);
       }
-      out += '</tr>';
 
-
-
-      for (var y = 0; y < 20; y++)
+      else 
       {
+            //Top row
             out += '<tr>';
-            out += '<td>' + (y+1) + '</td>';
-            for (var x = 0; x < 20; x++)
+            out += '<td></td>';
+            for (var x = 0; x < input; x++)
             {
-                  out += '<td>' + (x+1) * (y+1) + '</td>';
+            out += '<td>' + (x+1) + '</td>';
             }
             out += '</tr>';
+
+
+            //Creates rows
+            for (var y = 0; y < input; y++)
+            {
+                  out += '<tr>';
+                  out += '<td>' + (y+1) + '</td>';
+                  //Creates rows
+                  for (var x = 0; x < input; x++)
+                  {
+                        if(x==y)
+                        {
+                              out += '<td class="yellow">' + (x+1) * (y+1) + '</td>';
+                        }
+                        else
+                        {
+                              out += '<td>' + (x+1) * (y+1) + '</td>'; 
+                        }
+                  }
+                  out += '</tr>';
+            }
+
+            id.innerHTML=out;
       }
 
-      id.innerHTML=out;
+
 }
